7f4103f7f144edec2ba90cdc41be4c1f3c4276e8
yumaiwmt <yuma.iwamoto1@gmail.com>
2026-03-19 11:16:00 -0400

    Complete inference script for PatchCore

---
+++ scripts/inference_patchcore.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from anomalib.engine import Engine
+from anomalib.models import Patchcore
+
+
+def main():
+    base_path = Path(__file__).resolve().parent.parent
+
+    ckpt_path = base_path / "outputs_patchcore" / "v1" / "weights" / "lightning" / "model.ckpt"
+    images_to_test = base_path / "new_images"
+    results_root = base_path / "prediction_outputs_patchcore"
+
+    model = Patchcore()
+
+    engine = Engine(
+        default_root_dir=str(results_root),
+        accelerator="auto",
+        devices=1,
+    )
+
+    engine.predict(
+        model=model,
+        dataset=images_to_test,
+        ckpt_path=str(ckpt_path),
+    )
+
+
+if __name__ == "__main__":
+    main()
